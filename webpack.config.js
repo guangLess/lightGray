cf5234860ef59d30c5fce7a096214ca00d0eeaee
guangLess <bha.guang@gmail.com>
2018-01-28 23:46:30 -0500

    setup webpack, npm dependencies, etc

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,9 @@ const config = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'src')
+    },
     module: {
         rules: [
             {
